2fb77611559b8915a787bf1cd380e1a660c6d09f
Maria Bykova <mariab@bk.ru>
2022-10-25 12:54:27 +0300

    Промежуточный вариант (работа не доделана)

--- homework.py
+++ homework.py
@@ -1,11 +1,19 @@
-...
+import os
+import logging
+import requests
+import sys
+import time
+
+import telegram
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,38 +26,87 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+class ApiStatusCodeException(Exception):
+    """Ошибка запроса к основному API:
+    статус-код не равен 200."""
+    pass
+
+
+class HomeWorkIsNoneException(Exception):
+    """Отсутствует значение homework_name."""
+    pass
+
+
+class HomeworkStatusIsNoneException(Exception):
+    """У домашней работы отсутствует статус."""
+    pass
+
 
 def send_message(bot, message):
+    """Отправка сообщения."""
     ...
 
 
 def get_api_answer(current_timestamp):
+    """Получение данных от API Практикума."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    try:
+        homework_statuses = requests.get(
+            ENDPOINT, headers=HEADERS, params=params
+            )
+        if homework_statuses.status_code != 200:
+            error_message = (
+                f'Ошибка при запросе к основному API.'
+                f'Статус-код API: {homework_statuses.status_code}.'
+            )
+            logger.error(error_message)
+            raise ApiStatusCodeException(error_message)
+        return homework_statuses.json()
+    except Exception as error:
+        logger.error(f'Ошибка при запросе к основному API: {error}')
 
 
 def check_response(response):
-
+    
     ...
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_name is None:
+        error_message = 'Значение homework_name не найдено.'
+        logger.error(error_message)
+        raise HomeWorkIsNoneException(error_message)
+    if homework_status is None:
+        error_message = 'У домашней работы отсутствует статус.'
+        logger.error(error_message)
+        raise HomeworkStatusIsNoneException(error_message)
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    token = True
+    if PRACTICUM_TOKEN is None:
+        token = False
+        logger.critical('Отсутствует PRACTICUM_TOKEN')
+    if TELEGRAM_TOKEN is None:
+        token = False
+        logger.critical('Отсутствует TELEGRAM_TOKEN')
+    if TELEGRAM_CHAT_ID is None:
+        token = False
+        logger.critical('Отсутствует TELEGRAM_CHAT_ID')
+    return token
 
 
 def main():
@@ -64,11 +121,12 @@ def main():
 
     while True:
         try:
-            response = ...
+            response = get_api_answer(current_timestamp)
+            checked_homework = check_response(response)
 
             ...
 
-            current_timestamp = ...
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
 
         except Exception as error:
